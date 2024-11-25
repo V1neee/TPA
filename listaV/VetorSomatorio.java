4eb99a821a71277f2947285ba66c9a54b5f3a5bf
Vinicius de Jesus Chagas <vinicius.chagas@etec.sp.gov.br>
2024-11-25 08:58:13 -0300

    Add files via upload

---
+++ listaV/VetorSomatorio.java
@@ -0,0 +1,26 @@
+package listaV;
+import java.util.Scanner;
+public class VetorSomatorio {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		int i,a[],b[],soma;
+		final int TAM = 5;
+		a = new int[TAM];
+		b = new int[TAM];
+		for(i = 0;i<TAM;i++) {
+			System.out.println("Insira o "+(i + 1)+" numero");
+			a[i] = ler.nextInt();
+			soma = 0;
+			for(int j = 0;j<=a[i];j++) {
+				soma = j+soma;
+			}
+			b[i] = soma;
+		}
+		System.out.print("\nB = [ ");
+		for(i = 0;i<TAM;i++) {
+			System.out.print(b[i]+" ");
+		}
+		System.out.print("]");
+		ler.close();
+	}
+}
